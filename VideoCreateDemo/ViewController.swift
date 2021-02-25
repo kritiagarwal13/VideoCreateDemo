342521ff5ecd557e473056436bc01d1690e4dd4a
Kriti Agarwal <kriti.agarwal4709@gmail.com>
2021-02-25 16:21:20 +0530

    Initial Commit

---
+++ VideoCreateDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoCreateDemo
+//
+//  Created by Kriti Agarwal on 25/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
